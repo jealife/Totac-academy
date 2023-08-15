65d062af6a9bc52bec639fe76d58fc499d80063d
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-08-15 14:02:59 +0000

    Changes to be committed: 	modified:   src/components/home/Home.js 	modified:   src/components/home/home.css 	modified:   src/index.css

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,11 +2,23 @@ import './home.css';
 
 function Home() {
     return (
-        <div className='container'>
-            <section className='hero grid grid-center'>
-                <h1>Home</h1>
-            </section>
-        </div>
+
+        <section className=' container hero w-full grid  grid-col-2'>
+
+            <div className='left'>
+                <div className='left-content'>
+                    <h1>
+                        TOTAC un jour Totacien pour toujours
+                    </h1>
+                    <p>
+                        Nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut consequat semper viverra nam libero
+                    </p>
+                </div>
+            </div>
+            <div className='right'>
+
+            </div>
+        </section>
     )
 }
 
