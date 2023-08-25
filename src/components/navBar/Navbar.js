ad6d6a9499367652ab26ebf259567603d737cbea
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-08-25 22:14:13 +0000

    Changes to be committed: 	new file:   src/components/hero/Hero.js 	modified:   src/components/home/Home.js 	deleted:    src/components/home/home.css 	modified:   src/components/navBar/Navbar.js

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -38,7 +38,7 @@ export default function Navbar() {
             <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
                     <NavLink to='/' className="-m-1.5 p-1.5">
-                        <span className="sr-only">Your Company</span>
+                        <span className="sr-only">TOTAC Academy</span>
                         <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
                     </NavLink>
                 </div>
