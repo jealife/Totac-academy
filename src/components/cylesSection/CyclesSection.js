9e5549e705a4232f3d24f86f77d8fc3aafcebe5b
jealife <jeanguylanememiaghegmail.com>
2023-09-09 18:15:21 +0200

    Changes to be committed: 	modified:   src/components/cylesSection/CyclesSection.js

--- src/components/cylesSection/CyclesSection.js
+++ src/components/cylesSection/CyclesSection.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function CyclesSection() {
     return (
         <div className='w-full bg-indigo-100'>
-            <div className="mx-auto max-w-7xl px-6 lg:px-8 min-h-screen flex items-center justify-center " id='cyles'>
-                <h1 className='text-gray-900 font-semibold text-3xl'>Cycles de Formation</h1>
+            <div className="mx-auto max-w-7xl px-3 lg:px-8 min-h-screen flex items-center justify-center " id='cyles'>
+                <h1 className='text-gray-900 font-semibold text-3xl text-center'>Cycles de Formation</h1>
             </div>
         </div>
     )
