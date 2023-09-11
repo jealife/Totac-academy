b17938bb3b6c6268c90a39c8e8eaf648004517f3
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-09-11 12:03:24 +0000

    Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
 // ..
+AOS.init();
 
 
 function App() {
