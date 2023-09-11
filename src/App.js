4a16895f1537cfcd4bb8e7fd3991954e55bda13a
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-09-11 11:57:57 +0000

    modified:   src/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
-import ScrollToTop from './components/ScrollToTop';
-import { Fragment } from 'react';
 // ..
 AOS.init();
 
 
 function App() {
   return (
-    <Fragment>
-      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/a-propos" element={<About />} />
       </Routes>
-    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom';
 import Navbar from './components/navBar/Navbar';
 import 'transition-style';
+import ScrollToTop from './components/ScrollToTop';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <ScrollToTop />
     <Navbar/>
     <App />
   </BrowserRouter>
