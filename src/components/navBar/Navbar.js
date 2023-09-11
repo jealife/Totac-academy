9b483f95b8427a1262d7cc3d66fe4ab68d09e2bd
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-09-11 16:33:38 +0000

    modified:   src/components/navBar/Navbar.js

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -152,8 +152,8 @@ export default function Navbar() {
                 </div> */}
             </nav>
             <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                <div className="fixed inset-0 z-10" />
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                <div className="fixed inset-0 z-10 " />
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto backdrop-blur-xl bg-white/30 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <NavLink href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">TOTAC Academy</span>
@@ -201,13 +201,13 @@ export default function Navbar() {
                                     )}
                                 </Disclosure>
                                 <NavLink
-                                    href="#"
+                                    href="/Accompagnement"
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                 >
                                     Accompagnement
                                 </NavLink>
                                 <NavLink
-                                    href="#"
+                                    href="/Blog"
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                 >
                                     Blog
