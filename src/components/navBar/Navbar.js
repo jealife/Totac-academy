6db2077a8623e1524d7f5f7738dc698915db90cc
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-09-11 16:35:29 +0000

    modified:   src/components/blogSection/BlogSection.js 	modified:   src/components/navBar/Navbar.js

--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -207,7 +207,7 @@ export default function Navbar() {
                                     Accompagnement
                                 </NavLink>
                                 <NavLink
-                                    href="/Blog"
+                                    href="/"
                                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                 >
                                     Blog
