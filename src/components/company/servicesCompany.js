c66f0de4a22ff68dd55b50c7050dfb931315f2c5
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-09-11 00:38:17 +0000

    modified:   src/components/company/servicesCompany.js

--- src/components/company/servicesCompany.js
+++ src/components/company/servicesCompany.js
@@ -35,8 +35,8 @@ export default function ServicesCompany() {
                             "
                                 data-aos="fade-up"
                                 data-aos-duration="1000">
-                                <div className="rounded shadow-xl p-2" style={{ backgroundColor: "#ffffff", borderRadius: 9999, boxShadow: "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px" }}>
-                                    <img className='bg-gray-700' src={item.imageUrl} alt=""
+                                <div className="rounded-full bg-white shadow-xl p-2" style={{boxShadow: "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px" }}>
+                                    <img  src={item.imageUrl} alt=""
                                         width={200}
                                         height={200}
                                     />
