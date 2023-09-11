455b56d484167e308bdcadb54c305d31424ebfa8
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-09-11 11:53:15 +0000

    Changes to be committed: 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import { Routes,Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import About from './pages/about/About';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
 import ScrollToTop from './components/ScrollToTop';
+import { Fragment } from 'react';
 // ..
 AOS.init();
 
 
 function App() {
   return (
-    <Routes>
-      <ScrollToTop/>
-      <Route path="/" element={<Home/>} />
-      <Route path="/a-propos" element={<About/> } />
-    </Routes>
+    <Fragment>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/a-propos" element={<About />} />
+      </Routes>
+    </Fragment>
   );
 }
 
