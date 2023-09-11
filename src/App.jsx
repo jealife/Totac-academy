26a887c9ca3050defdbbe6b75e6f03b8fb774e45
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-09-11 11:47:46 +0000

    Changes to be committed: 	modified:   src/App.jsx 	new file:   src/components/ScrollToTop.js

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/home/Home';
 import About from './pages/about/About';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
+import ScrollToTop from './components/ScrollToTop';
 // ..
 AOS.init();
 
@@ -10,6 +11,7 @@ AOS.init();
 function App() {
   return (
     <Routes>
+      <ScrollToTop/>
       <Route path="/" element={<Home/>} />
       <Route path="/a-propos" element={<About/> } />
     </Routes>
