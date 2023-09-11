a7daf6cc79e31a1aba1f9879820eb0542437782a
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-09-11 18:49:36 +0000

    Changes to be committed:
    	modified:   src/components/blogSection/BlogSection.js

--- src/components/blogSection/BlogSection.js
+++ src/components/blogSection/BlogSection.js
@@ -62,7 +62,7 @@ export default function BlogSection() {
         <div className="bg-white py-32 sm:py-32">
             <div className="mx-auto max-w-7xl px-8 lg:px-10">
                 <div className="mx-auto max-w-2xl lg:mx-0">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Blog</h2>
                     <p className="mt-2 text-lg leading-8 text-gray-600">
                         Learn how to grow your business with our expert advice.
                     </p>
