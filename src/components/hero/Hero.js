c8b97092b1234532232b22a3b8bb8d81932a9c55
JEaLiFe <83034287+jealife@users.noreply.github.com>
2023-10-06 11:36:55 +0000

    Changes to be committed: 	new file:   src/assets/images/Graduate Cap (HD) (1).png 	new file:   src/assets/images/Graduation Certificate (HD).png 	new file:   src/assets/images/Holding Lightbulb.png 	new file:   src/assets/images/rocket.png 	modified:   src/components/hero/Hero.js 	modified:   src/components/logoCloud/LogoCloud.js

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,55 +1,76 @@
 import { Link } from "react-scroll";
-
-
-
-
+// import graduate from '../../assets/images/Graduation Certificate (HD).png'
+import HoldingLightBublb from "../../assets/images/Holding Lightbulb.png";
 
 export default function Hero() {
   return (
-
-    <div className='bg-white' id="hero">
+    <div className="bg-white" id="hero">
       <div className="relative isolate px-6 pt-14 pb-8 lg:px-8">
         <div
           className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-          aria-hidden="true">
+          aria-hidden="true"
+        >
           <div
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff953e] to-[#ff7607] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
-        <div className="mx-auto max-w-2xl pt-32  sm:py-48 lg:py-56">
-
+        <div className="mx-auto max-w-2xl pt-32  sm:py-48 lg:pt-36 lg:pb-0">
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
+            <h1
+              className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl"
               data-aos="fade-up"
               data-aos-duration="1000"
             >
               Se former pour se Transformer
             </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600"
-            data-aos="fade-up"
-            data-aos-duration="1000"
-            data-aos-delay="200" 
+            <p
+              className="mt-6 text-lg leading-8 text-gray-600"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay="200"
             >
-              À TOTAC, nous croyons profondément dans le pouvoir de la transformation personnelle et professionnelle à travers l’apprentissage et nous croyons en nos apprenants.
+              À TOTAC, nous croyons profondément dans le pouvoir de la
+              transformation personnelle et professionnelle à travers
+              l’apprentissage et nous croyons en nos apprenants.
             </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6"
-            data-aos="fade-up"
-            data-aos-duration="1000"
-            data-aos-delay="300" 
+            <div
+              className="mt-10 flex items-center justify-center gap-x-6"
+              data-aos="fade-up"
+              data-aos-duration="1000"
+              data-aos-delay="300"
             >
-              <Link to="cyles" spy={true} smooth={true} offset={-85} duration={500}
+              <Link
+                to="cyles"
+                spy={true}
+                smooth={true}
+                offset={-85}
+                duration={500}
                 className="rounded-md bg-[blue] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
               >
                 Formations
               </Link>
-              <a href="/" className="text-sm font-semibold leading-6 text-gray-900 hover:text-[orange] cursor-pointer">
-                En savoir plus sur la pédagogie TOTAC  <span aria-hidden="true" className="hover:text-[#ff7607]">→</span>
+              <a
+                href="/"
+                className="text-sm font-semibold leading-6 text-gray-900 hover:text-[orange] cursor-pointer"
+              >
+                En savoir plus sur la pédagogie TOTAC{" "}
+                <span aria-hidden="true" className="hover:text-[#ff7607]">
+                  →
+                </span>
               </a>
             </div>
+          </div>
+          <div
+            className="image-hero w-full flex items-center  justify-center pt-3"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-delay="300"
+          >
+            <img src={HoldingLightBublb} width={300} alt="" />
           </div>
         </div>
         <div
@@ -60,12 +81,11 @@ export default function Hero() {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#466bff] to-[#0400ff] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
-
--- src/components/logoCloud/LogoCloud.js
+++ src/components/logoCloud/LogoCloud.js
@@ -1,17 +1,16 @@
 export default function LogoCloud() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white py-24 sm:py-32 lg:py-20">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <h2 className="text-center text-2xl font-semibold leading-8 text-gray-900"
           data-aos="fade-up"
-          data-aos-duration="1000"
-          data-aos-delay="300"
+          data-aos-duration="600"
         >
           Ils font confiance à notre Académie
         </h2>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5"
           data-aos="fade-up"
-          data-aos-duration="500"
+          data-aos-duration="800"
         >
           <img
             className="col-span-2 max-h-12  object-contain lg:col-span-1"
